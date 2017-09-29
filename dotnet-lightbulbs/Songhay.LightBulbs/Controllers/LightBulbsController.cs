f010bd54c65cff8138d842e83393fd3dc0ec8d27
Bryan Wilhite <rasx@songhaysystem.com>
2017-09-29 15:44:28 -0700

    all tests pass

--- dotnet-lightbulbs/Songhay.LightBulbs/Controllers/LightBulbsController.cs
+++ dotnet-lightbulbs/Songhay.LightBulbs/Controllers/LightBulbsController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Songhay.LightBulbs.Models;
 
 namespace Songhay.LightBulbs.Controllers
 {
     [Route("api/[controller]")]
     public class LightBulbsController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public LightBulbsController(IRoomService roomService)
         {
-            return new string[] { "value1", "value2" };
+            this._roomService = roomService;
         }
+
+        [HttpGet("bulbs/{numberOfLightBulbs}/persons/{numberOfPersons}")]
+        public IEnumerable<LightBulb> Get(int numberOfLightBulbs, int numberOfPersons)
+        {
+            var bulbsOnByDefault = true;
+            return this._roomService.SwitchLightsWithAllPersons(numberOfLightBulbs, bulbsOnByDefault,numberOfPersons);
+        }
+
+        IRoomService _roomService;
     }
 }
