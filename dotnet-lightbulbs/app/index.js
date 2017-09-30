bc225e579bea05e853fd4ad1821b634dbcc62e71
Bryan Wilhite <bryan.wilhite@gmail.com>
2017-09-29 21:11:12 -0700

    client feature complete

--- dotnet-lightbulbs/app/index.js
+++ dotnet-lightbulbs/app/index.js
@@ -2,9 +2,41 @@
 /*jslint this, white, browser */
 /*global window, jQuery */
 
-(function ($) {
+(function($) {
     "use strict";
 
-    $(function () {
+    $(function() {
+        const form = $("#LightBulbForm");
+
+        form.validate({
+            errorClass: "has-error",
+            validClass: "has-success",
+            errorElement: "span",
+            errorPlacement: function(error, element) {
+                if (element.attr("type") != "number") {
+                    return;
+                }
+                let group = $(element).parent(".input-group");
+                error.addClass("help-block").insertAfter(group);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).parentsUntil(".form-group").addClass(errorClass).removeClass(validClass);
+                $("#runRoom").addClass("disabled");
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).parentsUntil(".form-group").removeClass(errorClass).addClass(validClass);
+            },
+            success: function() {
+                $("#runRoom").removeClass("disabled");
+            },
+            submitHandler: function(form) {
+                let numberOfLightBulbs = $("#numberOfLightBulbs").val();
+                let numberOfPersons = $("#numberOfPersons").val();
+                let uri = "http://localhost:5000/api/lightbulbs/" + numberOfLightBulbs + "/persons/" + numberOfPersons;
+                $.getJSON(uri, function(data) {
+                    console.log(data);
+                });
+            }
+        });
     });
 }(jQuery));
