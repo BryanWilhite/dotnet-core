ea70b2264f3dbd3f22a885ed534db182582354f5
Bryan Wilhite <rasx@songhaysystem.com>
2017-10-25 17:21:54 -0700

    config exploration

--- dotnet-console-configuration/Songhay.ConfigurationOne/Program.cs
+++ dotnet-console-configuration/Songhay.ConfigurationOne/Program.cs
@@ -19,12 +19,29 @@ namespace Songhay.ConfigurationOne
             //simple key-value pair:
             Console.WriteLine($"defaultTraceSourceName: {configuration["defaultTraceSourceName"]}");
 
+            //connection string convention:
+            var cnnKey = "contosoOne";
+            var cnn = configuration.GetConnectionString(cnnKey);
+            Console.WriteLine($"{cnnKey}: {cnn}");
+
             //binding to a dictionary:
             var set = new Dictionary<string, string>();
             configuration.GetSection("myDictionary").Bind(set);
             foreach (var pair in set)
             {
                 Console.WriteLine($"key: {pair.Key}, value: {pair.Value}");
+            }
+
+            //system environment variable:
+            var envKey = "PATH";
+            var path = configuration[envKey];
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.WriteLine($"{envKey}: [not found on this system]");
+            }
+            else
+            {
+                Console.WriteLine($"{envKey}: {path}");
             }
         }
     }
