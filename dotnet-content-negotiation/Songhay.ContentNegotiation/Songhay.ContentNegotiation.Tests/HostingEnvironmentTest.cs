c4a9a04d24f9524556ac8268912ca0251946673e
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-18 18:18:28 -0800

    :umbrella: ShouldGetWebHostBuilder() passes

--- dotnet-content-negotiation/Songhay.ContentNegotiation/Songhay.ContentNegotiation.Tests/HostingEnvironmentTest.cs
+++ dotnet-content-negotiation/Songhay.ContentNegotiation/Songhay.ContentNegotiation.Tests/HostingEnvironmentTest.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Songhay.ContentNegotiation.Tests.Extensions;
+using System.IO;
 
 namespace Songhay.ContentNegotiation.Tests
 {
@@ -8,8 +11,26 @@ namespace Songhay.ContentNegotiation.Tests
         public TestContext TestContext { get; set; }
 
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldGetWebHostBuilder()
         {
+            var basePath = this.TestContext.ShouldGetBasePath(this.GetType());
+            var builder = Program.GetWebHostBuilder(args: null);
+            Assert.IsNotNull(builder, "The expected Web Host builder is not here.");
+
+            builder.ConfigureAppConfiguration((builderContext, configBuilder) =>
+            {
+                Assert.IsNotNull(builderContext, "The expected Web Host builder context is not here.");
+
+                var env = builderContext.HostingEnvironment;
+                Assert.IsNotNull(env, "The expected Hosting Environment is not here.");
+
+                env.ContentRootPath = basePath;
+                env.EnvironmentName = "Development";
+                env.WebRootPath = $"{basePath}{Path.DirectorySeparatorChar}wwwroot";
+            });
+
+            var server = new TestServer(builder);
+            Assert.IsNotNull(server, "The expected test server is not here.");
         }
     }
 }
