93ba41e91143efb519de0ed527d6a6c803679bd5
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-18 16:30:39 -0800

    :sparkles: added dummy Contact repo

---
+++ dotnet-content-negotiation/Songhay.ContentNegotiation/Songhay.ContentNegotiation/Models/ContactRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Songhay.ContentNegotiation.Models
+{
+    public class ContactRepository : IContactRepository
+    {
+        static ContactRepository() => contacts = new ConcurrentDictionary<string, Contact>();
+
+        public ContactRepository()
+        {
+            Add(new Contact() { FirstName = "Nancy", LastName = "Davolio" });
+        }
+
+        public void Add(Contact contact)
+        {
+            contact.ID = Guid.NewGuid().ToString();
+            contacts[contact.ID] = contact;
+        }
+
+        public Contact Get(string id)
+        {
+            contacts.TryGetValue(id, out Contact contact);
+            return contact;
+        }
+
+        public IEnumerable<Contact> GetAll()
+        {
+            return contacts.Values;
+        }
+
+        public Contact Remove(string id)
+        {
+            contacts.TryRemove(id, out Contact contact);
+            return contact;
+        }
+
+        public void Update(Contact contact)
+        {
+            contacts[contact.ID] = contact;
+        }
+
+        static readonly ConcurrentDictionary<string, Contact> contacts;
+    }
+}
