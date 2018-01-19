1dbf6591495259ed3fc4708125071fac3b78f2fc
Bryan Wilhite <rasx@songhaysystem.com>
2018-01-19 13:12:19 -0800

    :umbrella: :microscope: ShouldGetContactsByDefaultAsJson() passes

--- dotnet-content-negotiation/Songhay.ContentNegotiation/Songhay.ContentNegotiation/Startup.cs
+++ dotnet-content-negotiation/Songhay.ContentNegotiation/Songhay.ContentNegotiation/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Songhay.ContentNegotiation.Models;
 using Songhay.ContentNegotiation.OutputFormatters;
 
 namespace Songhay.ContentNegotiation
@@ -25,10 +26,13 @@ namespace Songhay.ContentNegotiation
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options =>
-            {
-                options.OutputFormatters.Insert(0, new VcardOutputFormatter());
-            });
+            services
+                .AddSingleton<IContactRepository, ContactRepository>()
+                .AddMvc(options =>
+                {
+                    options.OutputFormatters.Add(new VcardOutputFormatter());
+                    //force vCard to be the default: options.OutputFormatters.Insert(0, new VcardOutputFormatter());
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
