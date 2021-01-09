a7a838de5ebf1529f3a881313024d56b3797fc7b
Bryan Wilhite <rasx@songhaysystem.com>
2021-01-08 20:44:44 -0800

    🚜🔥 csi-exe-minimal/

--- csi-exe-minimal/ps1/Download-NuGet.ps1
+++
@@ -1,7 +0,0 @@
-$outFolder = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Roslyn"
-
-Invoke-WebRequest `
-    -OutFile "$outFolder\nuget.exe" `
-    -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-
-Get-ChildItem -Path $outFolder -File nuget.exe
