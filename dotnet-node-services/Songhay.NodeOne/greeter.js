4779abcef6089e91d8fa98829e488af7872ecb97
Bryan Wilhite <rasx@songhaysystem.com>
2021-01-12 19:38:43 -0800

    🚜🔥 dotnet-node-services/

--- dotnet-node-services/Songhay.NodeOne/greeter.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="./node_modules/@types/node/index.d.ts" />
-module.exports = function(callback, name) {
-    var result = `Hello from Node, ${name}`;
-    callback( /* error */ null, result);
-}
