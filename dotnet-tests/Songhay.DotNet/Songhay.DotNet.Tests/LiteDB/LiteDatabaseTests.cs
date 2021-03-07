412a952793c425d2c5167d23e9849a68fd3f99d0
Bryan Wilhite <rasx@songhaysystem.com>
2021-03-07 12:37:13 -0800

    🖋

--- dotnet-tests/Songhay.DotNet/Songhay.DotNet.Tests/LiteDB/LiteDatabaseTests.cs
+++ dotnet-tests/Songhay.DotNet/Songhay.DotNet.Tests/LiteDB/LiteDatabaseTests.cs
@@ -25,46 +25,44 @@ namespace Songhay.DotNet.Tests.LiteDB
             Assert.True(projectFolderInfo.Exists);
 
             // Open database (or create if doesn't exist)
-            using (var db = new LiteDatabase(projectFolderInfo.ToCombinedPath(databaseFile)))
+            using var db = new LiteDatabase(projectFolderInfo.ToCombinedPath(databaseFile));
+            // Get a collection (or create, if doesn't exist)
+            var col = db.GetCollection<Customer>("customers");
+
+            // Create your new customer instance
+            var customer = new Customer
             {
-                // Get a collection (or create, if doesn't exist)
-                var col = db.GetCollection<Customer>("customers");
+                Name = "John Doe",
+                Phones = new string[] { "8000-0000", "9000-0000" },
+                IsActive = true
+            };
 
-                // Create your new customer instance
-                var customer = new Customer
-                {
-                    Name = "John Doe",
-                    Phones = new string[] { "8000-0000", "9000-0000" },
-                    IsActive = true
-                };
+            // Insert new customer document (Id will be auto-incremented)
+            col.Insert(customer);
 
-                // Insert new customer document (Id will be auto-incremented)
-                col.Insert(customer);
+            // Update a document inside a collection
+            customer.Name = "Jane Doe";
 
-                // Update a document inside a collection
-                customer.Name = "Jane Doe";
+            col.Update(customer);
 
-                col.Update(customer);
+            // Index document using document Name property
+            col.EnsureIndex(x => x.Name);
 
-                // Index document using document Name property
-                col.EnsureIndex(x => x.Name);
+            // Use LINQ to query documents (filter, sort, transform)
+            var results = col.Query()
+                .Where(x => x.Name.StartsWith("J"))
+                .OrderBy(x => x.Name)
+                .Select(x => new { x.Name, NameUpper = x.Name.ToUpper() })
+                .Limit(10)
+                .ToList();
 
-                // Use LINQ to query documents (filter, sort, transform)
-                var results = col.Query()
-                    .Where(x => x.Name.StartsWith("J"))
-                    .OrderBy(x => x.Name)
-                    .Select(x => new { x.Name, NameUpper = x.Name.ToUpper() })
-                    .Limit(10)
-                    .ToList();
+            // Let's create an index in phone numbers (using expression). It's a multikey index
+            col.EnsureIndex(x => x.Phones);
 
-                // Let's create an index in phone numbers (using expression). It's a multikey index
-                col.EnsureIndex(x => x.Phones);
+            // and now we can query phones
+            var r = col.FindOne(x => x.Phones.Contains("8888-5555"));
 
-                // and now we can query phones
-                var r = col.FindOne(x => x.Phones.Contains("8888-5555"));
-
-                Assert.Null(r);
-            }
+            Assert.Null(r);
         }
     }
 }
